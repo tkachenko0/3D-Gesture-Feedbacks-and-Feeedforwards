42377cd0c20472de02a3a2064c7d53836d0010e7
tkachenko0 <t.viacheslav00@gmail.com>
2023-12-09 13:45:33 +0100

    Multiple targets

--- feedforward/classification.py
+++ feedforward/classification.py
@@ -4,7 +4,7 @@ import numpy as np
 import mediapipe as mp
 
 
-def build_training_arrow_data(video_dir: str, window_len: int, target_joint: mp.solutions.holistic.PoseLandmark) -> None:
+def build_training_arrow_data(video_dir: str, window_len: int, target_joints: list[mp.solutions.holistic.PoseLandmark]) -> None:
 
     holistic = mp.solutions.holistic.Holistic()
 
@@ -29,17 +29,21 @@ def build_training_arrow_data(video_dir: str, window_len: int, target_joint: mp.
             results = holistic.process(frame_rgb)
 
             if results.pose_landmarks:
-                target_coords = results.pose_landmarks.landmark[target_joint]
+                data = []
+                for target_joint in target_joints:
+                    target_coords = results.pose_landmarks.landmark[target_joint]
+                    target_input = [target_coords.x,  target_coords.y]
+                    data += target_input
+
                 nose_coords = results.pose_landmarks.landmark[mp.solutions.holistic.PoseLandmark.NOSE]
-
-                target_input = [target_coords.x,  target_coords.y]
                 nose_input = [nose_coords.x, nose_coords.y]
-
-                frame_buffer.append(target_input + nose_input)
+                data += nose_input
+                
+                frame_buffer.append(data)
 
                 if len(frame_buffer) >= window_len:
                     X_training.append(frame_buffer[:int(window_len/2)])
-                    y_training.append(frame_buffer[-1][:2])
+                    y_training.append(frame_buffer[-1][:len(target_joints)*2])
                     frame_buffer = frame_buffer[1:]
 
             cv2.imshow('Video', frame)
